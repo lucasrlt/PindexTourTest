606b24deca45218fc28cd25accf2c9e87dcdb325
qtipee <quent.vallat@gmail.com>
2021-02-02 09:33:16 +0100

    Refactor home activity

--- app/src/main/java/com/pindex/main/home/HomeActivity.kt
+++ app/src/main/java/com/pindex/main/home/HomeActivity.kt
@@ -10,22 +10,42 @@ import com.pindex.main.decorators.MarginItemDecoration
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var itemsList: Array<String>
+    private lateinit var adapter: ItemAdapter
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val itemsList = Datasource(this).getItemsList()
+        setupData()
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        setRecyclerViewOnItemClick()
+    }
+
+    // Setup the main activity data
+    private fun setupData() {
+        // Get fake data from a XML file
+        itemsList = Datasource(this).getItemsList()
+
+        // Create the adapter
+        adapter = ItemAdapter(itemsList)
+
+        // Find the recycler view and bind it to the adapter
+        recyclerView = findViewById(R.id.recycler_view)
         recyclerView.addItemDecoration(MarginItemDecoration(
                 resources.getDimension(R.dimen.default_padding).toInt()))
-        val adapter = ItemAdapter(itemsList)
         recyclerView.adapter = adapter
+    }
 
+    // Set the on click event to the recycler view items
+    private fun setRecyclerViewOnItemClick() {
         adapter.onItemClick = { word ->
             val intent = Intent(this, TourActivity::class.java)
             intent.putExtra("title", word)
+
+            // Display the tour activity
             startActivity(intent)
         }
     }
