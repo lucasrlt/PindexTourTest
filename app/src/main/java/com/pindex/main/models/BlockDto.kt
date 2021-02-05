01450282d18a5c277bf1da08b752e478fc40151d
qtipee <quent.vallat@gmail.com>
2021-02-05 12:02:41 +0100

    Dynamically create an experience activity

--- app/src/main/java/com/pindex/main/models/BlockDto.kt
+++ app/src/main/java/com/pindex/main/models/BlockDto.kt
@@ -1,10 +1,12 @@
 package com.pindex.main.models
 
 import android.os.Parcelable
+import com.pindex.main.models.blocks.TextBlock
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class BlockDto(
-        var type: String? = null
+        var type: String? = null,
+        var sectionTitle: String ? = null,
+        var text: TextBlock? = null,
 )  : Parcelable
-
