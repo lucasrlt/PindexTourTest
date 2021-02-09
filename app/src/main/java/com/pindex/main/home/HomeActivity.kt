2402b04918eceb07ec3226f176afe444874b36b7
qtipee <quent.vallat@gmail.com>
2021-02-09 17:33:04 +0100

    MediaPlayer test via HTTP

--- app/src/main/java/com/pindex/main/home/HomeActivity.kt
+++ app/src/main/java/com/pindex/main/home/HomeActivity.kt
@@ -1,6 +1,10 @@
 package com.pindex.main.home
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.pindex.main.R
 import com.pindex.main.auth.PindexFirebase
@@ -13,6 +17,31 @@ class HomeActivity : AppCompatActivity() {
 
         // Anonymously log in the user to Firebase
         PindexFirebase.signInAnonymously(this)
+
+
+
+        // Test audio
+
+        val playerBtn = findViewById<Button>(R.id.player)
+        playerBtn.setOnClickListener {
+            /*
+            var mediaPlayer = MediaPlayer.create(this, R.raw.acid)
+            mediaPlayer.start()
+            */
+
+            val url = "https://safmarket.srvz-webapp.he-arc.ch/api/sample/file/1/0"
+            val mediaPlayer = MediaPlayer().apply {
+                setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build()
+                )
+                setDataSource((url))
+                prepare()
+                start()
+            }
+        }
     }
 
 }
