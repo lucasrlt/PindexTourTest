978bda05c7e93d1b430761e9cf5562c77eefb4f4
qtipee <quent.vallat@gmail.com>
2021-02-24 11:32:00 +0100

    Implement repository service pattern for experiences

--- app/src/main/java/com/pindex/main/repositories/ExperienceRepository.kt
+++ app/src/main/java/com/pindex/main/repositories/ExperienceRepository.kt
@@ -2,12 +2,15 @@ package com.pindex.main.repositories
 
 import com.pindex.main.models.ExperienceDto
 
+/**
+ * Experience repository interface.
+ */
 interface ExperienceRepository {
 
-    suspend fun getAll(): List<ExperienceDto>
-
-    suspend fun getPage(lastExperience: ExperienceDto? = null): List<ExperienceDto>
-
-    fun getById(id: String): ExperienceDto
+    /**
+     * Return a list of ExperienceDto whose size is defined
+     * by the given [limit].
+     */
+    suspend fun getPage(limit: Long): List<ExperienceDto>
 
 }
--- app/src/main/java/com/pindex/main/repositories/FirebaseExperienceRepository.kt
+++ app/src/main/java/com/pindex/main/repositories/FirebaseExperienceRepository.kt
@@ -1,91 +1,39 @@
 package com.pindex.main.repositories
 
-import android.util.Log
-import androidx.paging.PagingSource
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.firestore.ktx.toObject
 import com.pindex.main.models.ExperienceDto
 import com.pindex.main.utils.Constants
 import kotlinx.coroutines.tasks.await
-import java.lang.Exception
-import kotlin.math.exp
 
-class FirebaseExperienceRepository : /*ExperienceRepository,*/
-    PagingSource<ExperienceDto, ExperienceDto>() {
-
-    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
-/*
-    override suspend fun getAll(): List<ExperienceDto> {
-        return firestore.collection(Constants.FIRESTORE_EXPERIENCES_COLLECTION)
-            .get()
-            .await()
-            .toObjects(ExperienceDto::class.java)
-    }
-
+/**
+ * Experience repository that fetches the data from Firestore.
  */
-    /*
-    suspend fun getPage(lastExperience: ExperienceDto?): ExperienceDto {
-        var currentPage = firestore.collection((Constants.FIRESTORE_EXPERIENCES_COLLECTION))
-            .whereEqualTo("status", "listed")
-            .limit(Constants.FIRESTORE_QUERY_LIMIT)
+class FirebaseExperienceRepository : ExperienceRepository {
 
-        lastExperience?.let {
-            currentPage = currentPage.startAfter(lastExperience)
-        }
+    // Firestore instance
+    private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-        return currentPage.get()
-            //.await()
-            //.toObjects(ExperienceDto::class.java)
-    }
+    // Last experience used to fetch the next chunk of data
+    private var lastDocumentSnapshot: DocumentSnapshot? = null
 
-     */
-
-    /*
-    override fun getById(id: String): ExperienceDto {
-        TODO("Not yet implemented")
-    }
-
-     */
-
-    fun getPage(): List<ExperienceDto> {
-        val experiences = ArrayList<ExperienceDto>()
-
-        firestore.collection((Constants.FIRESTORE_EXPERIENCES_COLLECTION))
+    override suspend fun getPage(limit: Long): List<ExperienceDto> {
+        var query = firestore.collection(Constants.FIRESTORE_EXPERIENCES_COLLECTION)
                 .whereEqualTo("status", "listed")
-                .limit(Constants.FIRESTORE_QUERY_LIMIT)
-                .get()
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        for (document in task.result!!) {
-                            experiences.add(document.toObject(ExperienceDto::class.java))
-                            Log.d("BIBOU", document.toString())
-                        }
 
-                        Log.d("BIBOU", experiences.size.toString())
-
-                    }
-                }
-
-        return experiences
-    }
-
-    override suspend fun load(params: LoadParams<ExperienceDto>): LoadResult<ExperienceDto, ExperienceDto> {
-        return try {
-            val currentPage = getPage()
-
-            val nextPage = currentPage[currentPage.lastIndex]
-
-            LoadResult.Page(
-                data = currentPage,
-                prevKey = null,
-                nextKey = nextPage,
-            )
-        } catch (e: Exception) {
-            LoadResult.Error(e)
+        // Fetch the next chunk of data from the last experience
+        lastDocumentSnapshot?.let {
+            query = query.startAfter(it)
         }
 
-    }
+        val result = query
+                .limit(limit)
+                .get()
+                .await()
 
+        // Update the last experience
+        lastDocumentSnapshot = result.documents[result.size() - 1]
+
+        return result.toObjects(ExperienceDto::class.java)
+    }
 }
